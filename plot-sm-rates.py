6ff21acfcf4510e47665a37790037a92af89e792
alex <alex>
2019-05-29 17:48:49 +0200

    Adds 1-D distributions on each side

--- plot-sm-rates.py
+++ plot-sm-rates.py
@@ -10,33 +10,33 @@ import numpy as np
 from numpy.random import multivariate_normal
 import requests
 
-cookies = {
-    #'__cfduid': 'd5322ed8aff8f670be0782de50fde8d231557177073',
-    'referer_cookie': 'www.smartcat.ai',
-    #'uid': 'rB8AFFzQoWiIpRdsC8JYAg==',
-    'has_js': '1',
-    'tz': '0',
-}
+# cookies = {
+#     #'__cfduid': 'd5322ed8aff8f670be0782de50fde8d231557177073',
+#     'referer_cookie': 'www.smartcat.ai',
+#     #'uid': 'rB8AFFzQoWiIpRdsC8JYAg==',
+#     'has_js': '1',
+#     'tz': '0',
+# }
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0',
-    'Accept': 'application/json, text/plain, */*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Referer': 'https://www.smartcat.ai/',
-    'Content-Type': 'application/json;charset=utf-8',
-    'DNT': '1',
-    'Connection': 'keep-alive',
-    'TE': 'Trailers',
-}
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0',
+#     'Accept': 'application/json, text/plain, */*',
+#     'Accept-Language': 'en-US,en;q=0.5',
+#     'Referer': 'https://www.smartcat.ai/',
+#     'Content-Type': 'application/json;charset=utf-8',
+#     'DNT': '1',
+#     'Connection': 'keep-alive',
+#     'TE': 'Trailers',
+# }
 
-params = (
-    ('isNewSearchByUser', 'true'),
-    ('inAssignmentMode', 'false'),
-    #('random_lha53ft', ''),
-)
+# params = (
+#     ('isNewSearchByUser', 'true'),
+#     ('inAssignmentMode', 'false'),
+#     #('random_lha53ft', ''),
+# )
 
-data = '{accountId:null,searchString:,namePrefix:null,withoutAnyServices:false,withoutSpecifiedService:false,withPortfolio:false,searchMode:0,daytime:false,skip:0,limit:50,sortMode:1,restrictToUserIds:null,excludeUserIds:null,includeAllServices:false,serviceType:1,specializations:[],minRate:null,maxRate:null,specializationKnowledgeLevels:[],rateRangeCurrency:1,sourceLanguageId:9,targetLanguageId:12,expandLanguages:false,onlyNativeSpeakers:false}'
-data = '{"accountId":null,"searchString":,"namePrefix":null,"withoutAnyServices":false,"withoutSpecifiedService":false,"withPortfolio":false,"searchMode":0,"daytime":false,"skip":0,"limit":50,"sortMode":1,"restrictToUserIds":null,"excludeUserIds":null,"includeAllServices":false,"serviceType":1,"specializations":[],"minRate":null,"maxRate":null,"specializationKnowledgeLevels":[],"rateRangeCurrency":1,"sourceLanguageId":9,"targetLanguageId":12,"expandLanguages":false,"onlyNativeSpeakers":false}'
+# data = '{accountId:null,searchString:,namePrefix:null,withoutAnyServices:false,withoutSpecifiedService:false,withPortfolio:false,searchMode:0,daytime:false,skip:0,limit:50,sortMode:1,restrictToUserIds:null,excludeUserIds:null,includeAllServices:false,serviceType:1,specializations:[],minRate:null,maxRate:null,specializationKnowledgeLevels:[],rateRangeCurrency:1,sourceLanguageId:9,targetLanguageId:12,expandLanguages:false,onlyNativeSpeakers:false}'
+# data = '{"accountId":null,"searchString":,"namePrefix":null,"withoutAnyServices":false,"withoutSpecifiedService":false,"withPortfolio":false,"searchMode":0,"daytime":false,"skip":0,"limit":50,"sortMode":1,"restrictToUserIds":null,"excludeUserIds":null,"includeAllServices":false,"serviceType":1,"specializations":[],"minRate":null,"maxRate":null,"specializationKnowledgeLevels":[],"rateRangeCurrency":1,"sourceLanguageId":9,"targetLanguageId":12,"expandLanguages":false,"onlyNativeSpeakers":false}'
 
 
 #NB. Original query string below. It seems impossible to parse and
@@ -54,20 +54,25 @@ def main(args):
     volumes = []
 
     for vendor in json_response["results"]:
-        if vendor["translatedWordCount"] >= -1:
+        if vendor["translatedWordCount"] >= args.word_limit:
             rates.append(vendor["matchedService"]["pricePerUnit"])
             volumes.append(vendor["translatedWordCount"]+1)
 
     print (rates, len(rates))
-    #plt.hist(rates, bins=25, density=True, range=(0,0.16))
-    #plt.hist(np.log10(volumes), bins=25, density=True)
-    data = np.vstack([
-        rates,
-        np.log10(volumes)
-    ])
-    #plt.hist2d(data[:,0], data[:,1], range=((0,0.16),(0,7)))
-    plt.hist2d(rates, np.log10(volumes), range=((0,0.16),(0,7)))
-    plt.show()
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    axes[0,0].hist(rates, bins=20, range=(0,0.16))
+    axes[1,1].hist(np.log10(volumes), bins=20, orientation="horizontal")
+    axes[0,1].hist(rates, bins=20, weights=volumes, range=(0,0.16), density=True, histtype="stepfilled")
+    # axes[2,2].set_title("Rate vs Volume Distribution")
+    # axes[1,1].set_xlabel("Rate [$/word]")
+    # axes[1,1].set_ylabel("Volume [log10(words)]")
+    counts, xedges, yedges, im = axes[1,0].hist2d(rates, np.log10(volumes), range=((0,0.16),(0,7)))
+    # plt.colorbar(im)
+    axes[0,0].set_title("Rate [$/word]")
+    axes[1,0].set_ylabel("Volume [log10(words)]")
+    
+    plt.savefig(args.outfile)
+    plt.clf()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -80,9 +85,13 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '-o', '--outfile', dest='outfile',
-        type=argparse.FileType('w'),
-        nargs='?',
-        default=sys.stdout,
         help='Output file'
+    )
+    parser.add_argument(
+        "-w", "--word_limit", dest="word_limit",
+        type=int,
+        nargs="?",
+        default=-1,
+        help="Minimum number of words translated by a translator to be included"
     )
     main(parser.parse_args())
