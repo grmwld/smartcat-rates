9488ba496ad6bffddfc74ac91a11eea02463653f
Alex G <151401+agrimaldi@users.noreply.github.com>
2019-05-30 11:19:15 +0200

    Fixes filtering

--- plot-sm-rates.py
+++ plot-sm-rates.py
@@ -21,16 +21,14 @@ def main(args):
             rates.append(vendor["matchedService"]["pricePerUnit"])
             volumes.append(vendor["translatedWordCount"]+1)
 
-    print (rates, len(rates))
     fig, axes = plt.subplots(nrows=2, ncols=2)
-    axes[0,0].hist(rates, bins=20, range=(0,0.16))
-    axes[1,1].hist(np.log10(volumes), bins=20, orientation="horizontal")
-    axes[0,1].hist(rates, bins=20, weights=volumes, range=(0,0.16), density=True, histtype="stepfilled")
+    axes[0,0].hist(rates, bins=20, range=(0, args.max_rate))
+    axes[0,1].hist(rates, bins=20, weights=volumes, range=(0, args.max_rate), density=True, histtype="stepfilled")
+    counts, xedges, yedges, im = axes[1,0].hist2d(rates, np.log10(volumes), range=((0, args.max_rate), (np.log10(volumes).min(), 7)))
+    axes[1,1].hist(np.log10(volumes), bins=20, range=(np.log10(volumes).min(), 7), orientation="horizontal")
     # axes[2,2].set_title("Rate vs Volume Distribution")
     # axes[1,1].set_xlabel("Rate [$/word]")
     # axes[1,1].set_ylabel("Volume [log10(words)]")
-    counts, xedges, yedges, im = axes[1,0].hist2d(rates, np.log10(volumes), range=((0,0.16),(0,7)))
-    # plt.colorbar(im)
     axes[1,0].set_xlabel("Rate [$/word]")
     axes[1,0].set_ylabel("Volume [log10(words)]", rotation=90)
     # axes[1,1].yaxis.set_label_position("right")
@@ -58,5 +56,12 @@ if __name__ == '__main__':
         nargs="?",
         default=-1,
         help="Minimum number of words translated by a translator to be included"
+    )
+    parser.add_argument(
+        "-r", "--max_rate", dest="max_rate",
+        type=float,
+        nargs='?',
+        default=0.16,
+        help="Max rate"
     )
     main(parser.parse_args())
